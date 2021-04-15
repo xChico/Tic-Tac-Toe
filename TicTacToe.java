dac363a1241bad04a103fb78031a6d9104385b19
xChico <70347917+xChico@users.noreply.github.com>
2021-04-15 16:03:50 -0500

    Add files via upload

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,168 +1,187 @@
+//Miriam Chico
+//CS 304 Data Structures
+//Homework 1
 import java.util.Scanner;
 public class TicTacToe
 {
-   Scanner kbd= new Scanner(System.in);
+   Scanner kbd = new Scanner(System.in);
    private int size;
    private String[] playersNames = new String[2];
-   private String grid[][] = new String[size][size];
-   int turn=0;
+   private char[][] grid;
+   private int turn = 0;
+   int row=0;
+   int col=0;
+   
    public TicTacToe(int size, String[] playersNames)
    {
      this.size=size;
-     this.playersNames=playersNames;   
+     this.playersNames[0] = playersNames[0];
+     this.playersNames[1] = playersNames[1];
+     initializeGrid();
+
    }
    private void initializeGrid()
    {
+      grid = new char[size][size];
       for(int i=0; i<size; i++)
       {
          for(int j=0; j<size; j++)
-         { 
-            grid[i][j]= "-";
-         }   
+         {
+            grid[i][j]= '-';
+         }
       }
    }
    public void setPlayer()
    {
-
-      System.out.println(playersNames[(turn++)%2] + "'s your turn");
+      System.out.println(playersNames[(turn++)%2] + "'s your turn");   
    }
    public void play()
    {
-     int c=0;
-     initializeGrid();
-
-      //isFull();
       setPlayer();
       getPosition();
-      toString();
-      
-  
-     if(winner())
-      {System.out.println(playersNames[c%2] + " is a winner");
-     }
-     else if(isFull() && !winner()){
-      System.out.println("It's a tie");
-      c++;
+      System.out.print(this.toString());
+      if( isFull())
+      {
+         System.out.println( "It's a tie");
       }
+      else if(winner())
+      {
+         System.out.println(playersNames[turn++%2] + " is a winner");
+      } 
    }
    public void getPosition()
    {
-      System.out.println("Enter row and colum: ");
-      int row = kbd.nextInt();
-      int col = kbd.nextInt();
-      
-      isOccupied(grid, row, col);
-   }
-   public boolean isFull(String grid[][])
-   {
-      boolean full=false;
-      int count3=0;
-      for(int i=0; i<size; i++)
+     System.out.println("enter row and col: ");
+     row = kbd.nextInt();
+     col = kbd.nextInt();
+     if(isOccupied()==false)
       {
-         for(int j=0; j<size; j++)
+         if(this.turn%2 ==0)
          {
-            if(grid[i][j]=="-")
-            count3++;
-             if(count3 == (size*size)) 
-            full= true;
+            grid[row-1][col-1]='X';
          }
-         
+            else 
+         {
+            grid[row-1][col-1]='O'; 
+         }
       }
-     
-      return full;
    }
-   public boolean isOccupied(String grid2[][], int a, int b)
+   public boolean isFull()
    {
-     boolean occupied = false; 
-     if(grid2[a][b] == "O" || grid2[a][b]=="X")
+     boolean full = true;
+     for(int i=0; i<size; i++)
      {
-      System.out.println(occupied + "hmm, already taken, try again");
-     }
-     return occupied;
+      for(int j=0; j<size; j++)
+      {
+         if(grid[i][j]=='-')
+         {
+            full=false;
+         }
+      }
+     } 
+      return full;    
+   }
+   public boolean isOccupied()
+   {
+      boolean occupied= false;
+      if(grid[row-1][col-1] =='O' || grid[row-1][col-1] == 'X')
+      { 
+         System.out.println( "hmm, already taken, try again");
+         getPosition();
+         occupied= true;
+      }
+      return occupied;
    }
    public boolean winner()
    {
-     boolean winnerX=false;
-     boolean winnerO=false;
-      int count1;
-      int count2; 
-      for(int i=0; i<size; i++) 
+      boolean win=false;
+    
+      int count5=0;
+      int count7=0;
+      int count8=0;
+      int count6=0;
+      for(int i=0; i<size; i++)
       {
-      count1=0;
-      count2=0;
-        if(grid[i][0] == "O")
-        {count1++;}
-        else if(grid[i][0]=="X")
-        {count2++; }
-        if(count1==size)return winnerO;
-        else if(count2==size)return winnerX;
-      }    
-      for(int i=0; i<size; i++) 
+         int count1=0;
+         int count2=0;
+         for(int j=0; j<size; j++)
+         {
+            if(grid[i][j]=='O')
+               count1++;
+            else if(grid[i][j]=='X')
+               count2++;    
+         } 
+         if(count1==size) 
+            win=true;
+         else if(count2==size)
+            win=true;
+      }
+      for(int i=0; i<size; i++)
       {
-      count1=0;
-      count2=0;
+         int count3=0;
+         int count4=0;
+         for(int j=0; j<size; j++)
+         {
+            if(grid[j][i] == 'O')
+               count3++;
+            if(grid[j][i] == 'X')
+               count4++;     
+         }
+         if(count3==size) 
+         win=true;
+         else if(count4==size)
+         win=true;     
+      }
+      for(int i=0; i<size; i++)
+      {
          for(int j=0; j<size; j++)
          {
             if(i==j)
             {
-               if(grid[0][i] =="O")
-                  {count1++;}
-               else if(grid[0][i]=="X")
-                  {count2++;}
+               if(grid[i][j]=='O')
+                  count5++;
+               else if(grid[i][j] == 'X')
+                  count6++;
             }
          }
-          if(count1==size)return winnerO;
-        else if(count2==size)return winnerX;
       }
+      if(count5==size)
+         win=true;
+      else if(count6==size)
+         win=true;
+   
       for(int i=0; i<size; i++)
-      {  
-      count1=0;
-      count2=0;
+      {
          for(int j=0; j<size; j++)
          {
-            if(grid[i][j] =="O")
-            {count1++;}
-            else if(grid[i][j] =="X")
-            {count2++;}
-         }
-          if(count1==size)return winnerO;
-        else if(count2==size)return winnerX;
-   }
-      for(int i=0; i<size; i++)
-      {
-      count1=0;
-      count2=0;
-      for(int j=0; i<size; j++)
-      {
-         if((i+j)==(size-1))
-         {
-            if(grid[i][j]=="O")
-            {count1++;}
-             else if(grid[i][j]=="X")
-            { count2++;}
-         }
+            if((i+j)==(size-1))
+            {
+               if(grid[i][j]=='O')
+                  count7++;
+               else if(grid[i][j]=='X')
+                  count8++;
+            }
+         }  
       }
-       if(count1==size)return winnerO;
-        else if(count2==size)return winnerX;
-      }
+      if(count7==size)
+         win=true;
+      else if(count8==size)
+         win=true;
+      return win;
       
-      return winnerO || winnerX;
    }
    @Override
    public String toString()
    {
-      String str="";
-      System.out.println("                ");
+      String str=" ";
       for(int i=0; i<size; i++)
-      {
-         System.out.print(" ");
-         for(int j=0; j<size; j++)
          {
-            str= grid[i][j] + " ";
+            for(int j=0; j<size; j++)
+            {
+               str+=grid[i][j] + " " ;
+            }
+            str+="\n ";
          }
-         System.out.println();
-         System.out.println("              ");
-      }
-         return str ;
+      return str;
    }
 }
